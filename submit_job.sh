9e6c95492d14879b03f82aaba410d385bc90d89e
Afsana0304 <148911234+Afsana0304@users.noreply.github.com>
2025-03-09 21:16:49 +0100

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -3,13 +3,13 @@
 
 #SBATCH -A demelo                       # Account name
 #SBATCH --partition sorcery             # Partition name
-#SBATCH --time=32:00:00                 # Set a time limit of 1 hour
+#SBATCH --time=11:00:00                 # Set a time limit of 11 hour
 #SBATCH -C GPU_MEM:40GB                   # Request 1 GPU
 #SBATCH --mem=32GB                      # Request 50 GB of system memory
 #SBATCH --gpus=1
 
 #timestamp=$(date +%Y%m%d_%H%M%S)
-#SBATCH --output=/hpi/fs00/home/afsana.mimi/llama_project/output/job_output_hugging_face_data.txt  # Standard output
+#SBATCH --output=/hpi/fs00/home/afsana.mimi/llama_project/output/job_exp3_deepseek.txt  # Standard output
 
  
 # Activate the Python environment
@@ -18,4 +18,4 @@ source /hpi/fs00/home/afsana.mimi/llama_project/icl/bin/activate
 echo "done"
 
 # Run the Python script
-python -u /hpi/fs00/home/afsana.mimi/llama_project/more_test_example.py  #added u to see output
+python -u /hpi/fs00/home/afsana.mimi/llama_project/deep_seek_data_experiment.py  #added u to see output
